9c81f511835e040627b9c1f8a5a386445abdb8ce
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2024-12-10 07:38:21 +0900

    [level 1] Title: 흉부외과 또는 일반외과 의사 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -3,9 +3,6 @@
 - 진료과가 흉부외과(CS) or 일반외과(GS)
 - 고용일자 내림차순 정렬
 - 이름 내림차순 정렬
-
-WHERE절에서 OR 조건 작성 시 특수 연산자 IN을 사용하면 코드를 간결하게 작성 가능
-다만, 프로그래머스에서는 IN 연산자가 작동하지 않으므로 OR로 제출할 것 
 */
 
 SELECT  DR_NAME
@@ -13,7 +10,9 @@ SELECT  DR_NAME
         ,MCDP_CD
         ,DATE_FORMAT(HIRE_YMD, '%Y-%m-%d')
   FROM  DOCTOR
- WHERE  MCDP_CD IN ('CS', 'GS')
+ # WHERE  MCDP_CD IN ('CS', 'GS')
+ WHERE  MCDP_CD = 'CS'
+    OR  MCDP_CD = 'GS'
  ORDER
     BY  HIRE_YMD DESC
         ,DR_NAME ASC;
