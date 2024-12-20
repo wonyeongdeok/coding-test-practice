b3f125ef73a5fc11a5005eb83a2c8a09b394dbe6
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2024-12-20 18:10:46 +0900

    Time: 841 ms (10.66%), Space: 0B (100.00%) - LeetHub

---
+++ LeetCode/1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,16 @@
+/*
+print: sell_date, num_sold, products
+condition: sold products names should be sorted lexicographically
+order: sell_date asc
+*/
+
+SELECT
+    SELL_DATE,
+    COUNT(DISTINCT PRODUCT) AS NUM_SOLD,
+    GROUP_CONCAT(DISTINCT PRODUCT ORDER BY PRODUCT SEPARATOR ',') AS PRODUCTS
+FROM
+    ACTIVITIES
+GROUP BY
+    SELL_DATE
+ORDER BY
+    SELL_DATE ASC;
