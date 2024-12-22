485c45d5e7ba21bbdd85ae2dc452c5f4df1eaf88
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2024-12-22 21:38:34 +0900

    Time: 876 ms (62.89%), Space: 0B (100%) - LeetHub

--- LeetCode/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ LeetCode/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -5,7 +5,12 @@ condition:
  - use DELETE
 */
 
-DELETE p1
-FROM Person p1
-JOIN Person p2
-ON p1.email = p2.email AND p1.id > p2.id;
+DELETE
+    P1
+FROM
+    PERSON P1
+JOIN
+    Person P2
+ON P1.EMAIL = P2.EMAIL
+WHERE
+    P1.ID > P2.ID;
