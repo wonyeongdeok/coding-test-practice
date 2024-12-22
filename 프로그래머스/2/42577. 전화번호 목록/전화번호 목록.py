5e59647094eba9b9b70ec45f3759a4b2c4218867
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2024-12-22 18:33:12 +0900

    [level 2] Title: 전화번호 목록, Time: 497.74 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,11 @@
 def solution(phone_book):
+    answer = True
     # 해시 테이블 생성
     phone_hash = {phone: True for phone in phone_book}
-    
-    # 각 전화번호에 대해 접두어 확인
+    # 각 전화번호의 접두어가 해시 테이블에 존재하는지 확인
     for phone in phone_book:
-        # 전화번호의 각 접두어를 확인
         for i in range(1, len(phone)):
-            if phone[:i] in phone_hash:  # 접두어가 해시에 존재한다면
-                return False
+            if phone[: i] in phone_hash:
+                answer = False 
     
-    return True
+    return answer
