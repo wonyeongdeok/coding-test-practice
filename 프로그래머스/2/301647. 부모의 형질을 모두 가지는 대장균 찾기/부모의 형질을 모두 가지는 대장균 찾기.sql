0767125ec9971e1444050197b3b515aeb6b21234
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2024-12-22 15:29:18 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -1,20 +1,18 @@
 /*
-출력: 대장균의 id, 대장균의 형질, 부모 대장균의 형질
-조건: 부모의 형질을 모두 보유한 대장균의 id
-정렬: id 오름차순
+출력: ID, GENOTYPE, PARENT_GENOTYPE
+조건: 자식 대장균은 부모 대장균의 형질을 모두 보유
+정렬: ID 오름차순
 */
 
-SELECT 
-    e1.ID, 
-    e1.GENOTYPE, 
-    e2.GENOTYPE AS PARENT_GENOTYPE
-FROM 
-    ECOLI_DATA e1
-JOIN 
-    ECOLI_DATA e2
-ON 
-    e1.PARENT_ID = e2.ID
-WHERE 
-    (e1.GENOTYPE | e2.GENOTYPE) = e1.GENOTYPE
-ORDER BY 
-    e1.ID;
+SELECT
+    C.ID,
+    C.GENOTYPE,
+    P.GENOTYPE AS PARENT_GENOTYPE
+FROM
+    ECOLI_DATA C
+INNER JOIN
+    ECOLI_DATA P ON C.PARENT_ID = P.ID
+WHERE
+    C.GENOTYPE | P.GENOTYPE = C.GENOTYPE
+ORDER BY
+    C.ID;
