f496a9003cba1528043eb0e3186e0adcdf98c41f
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2024-12-24 13:23:35 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,30 @@
+/*
+print: day, active_users
+condition:
+ - period of 30 days ending 2019-07-27
+*/
+
+-- get users within period
+WITH WITHIN_PERIOD_USERS AS (
+    SELECT
+        USER_ID,
+        ACTIVITY_DATE
+    FROM
+        ACTIVITY
+    WHERE
+        ACTIVITY_DATE BETWEEN DATE_SUB('2019-07-27', INTERVAL 30 DAY) AND '2019-07-27'
+),
+GROUP_DAYS AS (
+    SELECT
+        ACTIVITY_DATE AS DAY,
+        COUNT(DISTINCT USER_ID) AS ACTIVE_USERS
+    FROM
+        WITHIN_PERIOD_USERS
+    GROUP BY
+        ACTIVITY_DATE    
+)
+SELECT
+    DAY,
+    ACTIVE_USERS
+FROM
+    GROUP_DAYS;
