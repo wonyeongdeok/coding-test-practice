d549f2eb64d1a3416f27e5084ef89d8a3343d5ef
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2024-12-24 13:46:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -0,0 +1,16 @@
+/*
+print: user_id, name, mail
+condition:
+ - mail is valid
+  - upper case, lower case, digits, '_', '.', '-'
+  - satrt with a letter
+  - domain is '@leetcode.com'
+*/
+SELECT
+    USER_ID,
+    NAME,
+    MAIL
+FROM
+    USERS
+WHERE
+    MAIL REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode.com$';
