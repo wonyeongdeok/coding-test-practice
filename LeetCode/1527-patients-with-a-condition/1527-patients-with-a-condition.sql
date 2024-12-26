102d3851569b420b34a4265657045646f5af94cf
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2024-12-26 10:19:02 +0900

    Time: 1278 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -0,0 +1,13 @@
+/*
+print: patient_id, patient_name, conditions
+condition:
+ - have type 1 diabetes that starts with DIAB1
+*/
+SELECT
+    PATIENT_ID,
+    PATIENT_NAME,
+    CONDITIONS
+FROM
+    PATIENTS
+WHERE
+    CONDITIONS REGEXP '(^| )DIAB1[0-9]*( |$)';
