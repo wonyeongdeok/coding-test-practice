beaed4bc53a41f2d121edc8df41d08e60bc406c1
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2024-12-27 12:56:03 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,18 @@
+/*
+출력: ANIMAL_ID, NAME
+조건:
+ - 입양 간 동물 대상
+ - 보호 기간이 가장 긴 두 마리
+정렬: 보호기간 desc
+*/
+
+SELECT
+    O.ANIMAL_ID,
+    O.NAME
+FROM
+    ANIMAL_INS I
+RIGHT JOIN
+    ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+ORDER BY
+    DATEDIFF(O.DATETIME, I.DATETIME) DESC
+LIMIT 2;
