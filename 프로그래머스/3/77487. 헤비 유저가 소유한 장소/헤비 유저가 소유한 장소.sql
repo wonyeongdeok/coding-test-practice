3d6e72f790b51cb80107f4386bb281114b4be03c
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-01-03 15:02:06 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,26 @@
+/*
+출력: ID, NAME, HOST_ID
+조건:
+ - 헤비유저 찾기
+ - 헤비유저는 공간을 둘 이상 등록한 사람
+정렬: ID asc
+*/
+
+WITH HEAVY AS (
+    SELECT
+        HOST_ID
+    FROM
+        PLACES
+    GROUP BY
+        HOST_ID
+    HAVING
+        COUNT(*) >= 2
+)
+SELECT
+    ID,
+    NAME,
+    HOST_ID
+FROM
+    PLACES
+WHERE
+    HOST_ID IN (SELECT HOST_ID FROM HEAVY)
