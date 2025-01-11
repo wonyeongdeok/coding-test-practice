4c40292780f97bb8df28cca171852eb019c38225
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-01-11 22:15:07 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -6,27 +6,64 @@
  - 전화번호는 xxx-xxxx-xxxx
 정렬: USER_ID DESC
 */
-SELECT
-    U.USER_ID,
-    U.NICKNAME,
-    CONCAT_WS(' ',
-              U.CITY,
-              U.STREET_ADDRESS1,
-              U.STREET_ADDRESS2
-    ) AS '전체주소',
-    CONCAT(SUBSTRING(U.TLNO, 1, 3),
-           '-',
-           SUBSTRING(U.TLNO, 4, 4),
-           '-',
-           SUBSTRING(U.TLNO, 8, 4)
-    ) AS '전화번호'
-FROM
-    USED_GOODS_BOARD B
-JOIN
-    USED_GOODS_USER U ON B.WRITER_ID = U.USER_ID
-GROUP BY
-    U.USER_ID
-HAVING
-    COUNT(*) >= 3
-ORDER BY
-    USER_ID DESC;
+# SELECT
+#     U.USER_ID,
+#     U.NICKNAME,
+#     CONCAT_WS(' ',
+#               U.CITY,
+#               U.STREET_ADDRESS1,
+#               U.STREET_ADDRESS2
+#     ) AS '전체주소',
+#     CONCAT(SUBSTRING(U.TLNO, 1, 3),
+#            '-',
+#            SUBSTRING(U.TLNO, 4, 4),
+#            '-',
+#            SUBSTRING(U.TLNO, 8, 4)
+#     ) AS '전화번호'
+# FROM
+#     USED_GOODS_BOARD B
+# JOIN
+#     USED_GOODS_USER U ON B.WRITER_ID = U.USER_ID
+# GROUP BY
+#     U.USER_ID
+# HAVING
+#     COUNT(*) >= 3
+# ORDER BY
+#     USER_ID DESC;
+    
+    
+    
+# SELECT  U.USER_ID
+#         ,U.NICKNAME
+#         ,CONCAT(U.CITY, ' ', U.STREET_ADDRESS1, U.STREET_ADDRESS2) AS 전체주소
+#         ,CONCAT(SUBSTR(U.TLNO,1,3), '-', SUBSTR(U.TLNO,4,4), '-', SUBSTR(U.TLNO,8,4)) AS 전화번호
+#   FROM  USED_GOODS_BOARD AS B
+#   JOIN  USED_GOODS_USER AS U
+#     ON  B.WRITER_ID = U.USER_ID
+#  GROUP
+#     BY  U.USER_ID
+# HAVING  COUNT(B.BOARD_ID) >= 3
+#  ORDER
+#     BY  U.USER_ID;
+    
+
+    
+    
+    
+    
+    
+SELECT  U.USER_ID,
+        U.NICKNAME,
+        CONCAT(U.CITY, ' ', U.STREET_ADDRESS1, ' ', IFNULL(U.STREET_ADDRESS2, '')) AS 전체주소,
+        CONCAT(SUBSTRING(U.TLNO, 1, 3), '-', SUBSTRING(U.TLNO, 4, 4), '-', SUBSTRING(U.TLNO, 8, 4)) AS 전화번호
+  FROM  USED_GOODS_USER AS U
+  JOIN  (SELECT  WRITER_ID,
+                 COUNT(BOARD_ID) AS TOTAL_POSTS
+           FROM  USED_GOODS_BOARD
+          GROUP
+             BY  WRITER_ID
+         HAVING  TOTAL_POSTS >= 3
+    ) AS B
+    ON  U.USER_ID = B.WRITER_ID
+ ORDER
+    BY  U.USER_ID DESC;
