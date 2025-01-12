6fbfb6f5941b5b84bc320b6e427389573b804508
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-01-12 19:38:13 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,11 +1,10 @@
 /*
 출력: ANIMAL_ID, ANIMAL_TYPE, NAME
 조건:
- - 보호소에들어올 당시 중성화되지 않았지만, 보호소 나갈 당시 중성화된 동물
- - 중성화 X -> 성별 및 중성화 여부에 Intact 표시
- - 중성화 O -> 성별 및 중성화 여부에 Spayed or Neutered
+ - 보호소에 들어올 당시 중성화 X
+ - 보호소 나갈 당시 중성화 O
+정렬: ANIMAL_ID
 */
-
 SELECT
     I.ANIMAL_ID,
     I.ANIMAL_TYPE,
@@ -15,5 +14,7 @@ FROM
 JOIN
     ANIMAL_OUTS AS O ON I.ANIMAL_ID = O.ANIMAL_ID
 WHERE
-    I.SEX_UPON_INTAKE LIKE '%Intact%'
-    AND (O.SEX_UPON_OUTCOME LIKE '%Spayed%' OR O.SEX_UPON_OUTCOME LIKE '%Neutered%')
+    I.SEX_UPON_INTAKE REGEXP 'Intact'
+    AND (O.SEX_UPON_OUTCOME REGEXP 'Spayed|Neutered')
+ORDER BY
+    I.ANIMAL_ID;
