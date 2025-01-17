4c02f4b763e728c64266b894e67623ce2ec8ef79
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-01-17 10:46:11 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,20 +1,51 @@
-SELECT  B.HISTORY_ID
-        ,FLOOR(A.DAILY_FEE * (1 - IFNULL(C.DISCOUNT_RATE, 0) * 0.01) * B.DAY_DIFF) AS FEE
-  FROM  CAR_RENTAL_COMPANY_CAR AS A
- RIGHT
-  JOIN  (SELECT  *
-                 ,DATEDIFF(END_DATE, START_DATE) + 1 AS DAY_DIFF  -- 빌린 날 포함해서 요금 부과
-                 ,CASE WHEN DATEDIFF(END_DATE, START_DATE) >= 90 THEN '90일 이상'
-                       WHEN DATEDIFF(END_DATE, START_DATE) >= 30 THEN '30일 이상'
-                       WHEN DATEDIFF(END_DATE, START_DATE) >= 7 THEN '7일 이상'
-                       END AS DURATION_TYPE
-           FROM  CAR_RENTAL_COMPANY_RENTAL_HISTORY) AS B
-    ON  A.CAR_ID = B.CAR_ID
-  LEFT
-  JOIN  CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS C
-    ON  A.CAR_TYPE = C.CAR_TYPE
-   AND  B.DURATION_TYPE = C.DURATION_TYPE
- WHERE  A.CAR_TYPE = '트럭'
- ORDER
-    BY  FEE DESC
-        ,B.HISTORY_ID DESC;
+/*
+출력: HISTORY_ID, FEE
+조건:
+ - FEE 정수부분만 출력
+ - 자동차 종류 '트럭'
+ - 대여 기록별 대여 금액 구하기
+정렬:
+ - FEE DESC
+ - ID DESC
+*/
+
+-- 자동차 종류가 트럭인 대여 기록의 대여 기간 및 대여 타입 구하기
+WITH RENTAL_DURATION AS (
+    SELECT
+        H.HISTORY_ID,
+        C.CAR_TYPE,
+        C.DAILY_FEE,
+        DATEDIFF(H.END_DATE, H.START_DATE) + 1 AS DURATION,
+        CASE
+            WHEN DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 90 THEN '90일 이상'
+            WHEN DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 30 THEN '30일 이상'
+            WHEN DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 7 THEN '7일 이상'
+            ELSE NULL
+        END AS DURATION_TYPE
+    FROM
+        CAR_RENTAL_COMPANY_CAR AS C
+    JOIN
+        CAR_RENTAL_COMPANY_RENTAL_HISTORY AS H ON C.CAR_ID = H.CAR_ID
+    WHERE
+        C.CAR_TYPE = '트럭'
+),
+RENTAL_FEE AS (
+    SELECT
+        R.HISTORY_ID,
+        FLOOR(
+            R.DAILY_FEE * (1 - (0.01 * IFNULL(P.DISCOUNT_RATE, 0))) * DURATION
+        )AS FEE
+    FROM
+        RENTAL_DURATION AS R
+    LEFT JOIN
+        CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS P ON R.CAR_TYPE = P.CAR_TYPE
+                                              AND R.DURATION_TYPE = P.DURATION_TYPE
+)
+SELECT
+    HISTORY_ID,
+    FEE
+FROM
+    RENTAL_FEE
+ORDER BY
+    FEE DESC,
+    HISTORY_ID DESC;
