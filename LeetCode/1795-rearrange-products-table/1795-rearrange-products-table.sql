d79f6050e66d224e998e2d4afc046de72690f87f
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-01-18 16:20:04 +0900

    Time: 794 ms (46.42%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,35 @@
+/*
+PRINT: PRODUCT_ID, STORE, PRICE
+CONDITION:
+ - if a product is not available in a store, don`t print
+*/
+SELECT
+    PRODUCT_ID,
+    'store1' AS STORE,
+    STORE1 AS PRICE
+FROM
+    PRODUCTS
+WHERE
+    STORE1 IS NOT NULL
+
+UNION ALL
+
+SELECT
+    PRODUCT_ID,
+    'store2' AS STORE,
+    STORE2 AS PRICE
+FROM
+    PRODUCTS
+WHERE
+    STORE2 IS NOT NULL
+
+UNION ALL
+
+SELECT
+    PRODUCT_ID,
+    'store3' AS STORE,
+    STORE3 AS PRICE
+FROM
+    PRODUCTS
+WHERE
+    STORE3 IS NOT NULL;
