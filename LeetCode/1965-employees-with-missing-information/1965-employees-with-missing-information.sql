45104ede42dd8cd4da7f759c3b24f30cf7392186
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-01-21 09:46:33 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,38 @@
+/*
+print: EMPLOYEE_ID
+condition:
+ - employees with missing information about name, salary
+order: employee_id
+*/
+
+WITH MISSING_SALARY AS (
+    SELECT
+        E.EMPLOYEE_ID
+    FROM
+        EMPLOYEES AS E
+    LEFT JOIN
+        SALARIES AS S ON E.EMPLOYEE_ID = S.EMPLOYEE_ID
+    WHERE
+        S.SALARY IS NULL
+),
+MISSING_NAME AS (
+    SELECT
+        E.EMPLOYEE_ID
+    FROM
+        EMPLOYEES AS E
+    RIGHT JOIN
+        SALARIES AS S ON E.EMPLOYEE_ID = S.EMPLOYEE_ID
+    WHERE
+        E.NAME IS NULL
+)
+SELECT
+    EMPLOYEE_ID
+FROM
+    MISSING_SALARY
+-- UNION
+-- SELECT
+--     EMPLOYEE_ID
+-- FROM
+--     MISSING_NAME
+-- ORDER BY
+--     EMPLOYEE_ID;
