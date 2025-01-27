c7a26e11f58499f7fa2c44bfe4c0f88562daa35a
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-01-27 14:39:01 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,15 @@
+/*
+print: CUSTOMER_ID
+conditions: 
+ - customer who bought all the products
+*/
+SELECT
+    C.CUSTOMER_ID
+FROM
+    CUSTOMER AS C
+JOIN
+    PRODUCT AS P ON C.PRODUCT_KEY = P.PRODUCT_KEY
+GROUP BY
+    C.CUSTOMER_ID
+HAVING
+    COUNT(*) = (SELECT COUNT(PRODUCT_KEY) FROM PRODUCT);
