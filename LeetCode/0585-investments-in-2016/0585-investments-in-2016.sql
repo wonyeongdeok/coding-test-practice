b2c6e764c6be0d1c9ca47223ac7fbe121178418c
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-02-03 21:01:05 +0900

    Time: 541 ms (90.25%), Space: 0B (100%) - LeetHub

--- LeetCode/0585-investments-in-2016/0585-investments-in-2016.sql
+++ LeetCode/0585-investments-in-2016/0585-investments-in-2016.sql
@@ -6,6 +6,39 @@ conditions:
  - policyholders are not loacted in the same city as any other policyholder attribute pairs must be unique.
  - round to two decimal
 */
+-- SOL1 --
+-- WITH FIRST_CONDITION AS (
+--     SELECT
+--         TIV_2015
+--     FROM
+--         INSURANCE
+--     GROUP BY
+--         TIV_2015
+--     HAVING
+--         COUNT(*) >= 2
+-- ),
+-- SECOND_CONDITION AS (
+--     SELECT
+--         LAT,
+--         LON
+--     FROM
+--         INSURANCE
+--     GROUP BY
+--         LAT,
+--         LON
+--     HAVING
+--         COUNT(*) = 1
+-- )
+-- SELECT
+--     ROUND(SUM(TIV_2016), 2) AS TIV_2016
+-- FROM
+--     INSURANCE
+-- WHERE
+--     TIV_2015 IN (SELECT TIV_2015 FROM FIRST_CONDITION)
+--     AND LAT IN (SELECT LAT FROM SECOND_CONDITION)
+--     AND LON IN (SELECT LON FROM SECOND_CONDITION);
+
+-- SOL2 --
 WITH FIRST_CONDITION AS (
     SELECT
         TIV_2015
@@ -31,8 +64,9 @@ SECOND_CONDITION AS (
 SELECT
     ROUND(SUM(TIV_2016), 2) AS TIV_2016
 FROM
-    INSURANCE
-WHERE
-    TIV_2015 IN (SELECT TIV_2015 FROM FIRST_CONDITION)
-    AND LAT IN (SELECT LAT FROM SECOND_CONDITION)
-    AND LON IN (SELECT LON FROM SECOND_CONDITION);
+    INSURANCE AS I
+JOIN
+    FIRST_CONDITION AS F ON I.TIV_2015 = F.TIV_2015
+JOIN
+    SECOND_CONDITION AS S ON I.LAT = S.LAT
+                          AND I.LON = S.LON;
