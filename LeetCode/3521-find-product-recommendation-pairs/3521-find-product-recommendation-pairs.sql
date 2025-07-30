9676b0487b9b87d0733599119edaf85527445c22
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-07-30 09:28:11 +0900

    Time: 844 ms (11.24%), Space: 0B (100%) - LeetHub

--- LeetCode/3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ LeetCode/3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -1,26 +1,27 @@
-/*
-1. Identify distinct product pairs frequently purchased together by the same customers
-2. For each product pair, determine how many customers purchased both products
-3. A product pair is considered for recommendation if at least 3 different customers have purchased both products.
-4. order by customer_count desc, product1_id asc, product2_id asc
-*/
-
+WITH pairs AS (
+    SELECT
+        a.product_id AS product1_id,
+        b.product_id AS product2_id,
+        COUNT(*) AS customer_count
+    FROM
+        ProductPurchases a
+    JOIN
+        ProductPurchases b ON a.user_id = b.user_id
+                           AND a.product_id < b.product_id
+    GROUP BY
+        a.product_id,
+        b.product_id
+    HAVING
+        COUNT(*) >= 3
+)
 SELECT
-    a.product_id AS product1_id,
-    b.product_id AS product2_id,
+    product1_id,
+    product2_id,
     (SELECT category FROM ProductInfo WHERE product1_id = product_id) AS product1_category,
     (SELECT category FROM ProductInfo WHERE product2_id = product_id) AS product2_category,
-    COUNT(*) AS customer_count
+    customer_count
 FROM
-    ProductPurchases a
-JOIN
-    ProductPurchases b ON a.user_id = b.user_id
-                        AND a.product_id < b.product_id
-GROUP BY
-    a.product_id,
-    b.product_id
-HAVING
-    COUNT(*) >= 3
+    pairs
 ORDER BY
     customer_count DESC,
     product1_id,
