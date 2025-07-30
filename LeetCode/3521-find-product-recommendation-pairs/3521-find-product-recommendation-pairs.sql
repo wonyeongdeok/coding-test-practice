012ea83ca82ca829798c37c61a7fa052b76ac62e
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-07-30 09:24:30 +0900

    Time: 546 ms (90.27%), Space: 0B (100%) - LeetHub

--- LeetCode/3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ LeetCode/3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -5,31 +5,22 @@
 4. order by customer_count desc, product1_id asc, product2_id asc
 */
 
--- 유저별 상품 쌍 만들기
-WITH pairs AS (
-    SELECT
-        a.product_id AS product1_id,
-        b.product_id AS product2_id,
-        COUNT(*) AS customer_count
-    FROM
-        ProductPurchases a
-    JOIN
-        ProductPurchases b ON a.user_id = b.user_id
-                            AND a.product_id < b.product_id
-    GROUP BY
-        a.product_id,
-        b.product_id
-    HAVING
-        COUNT(*) >= 3
-)
 SELECT
-    product1_id,
-    product2_id,
+    a.product_id AS product1_id,
+    b.product_id AS product2_id,
     (SELECT category FROM ProductInfo WHERE product1_id = product_id) AS product1_category,
     (SELECT category FROM ProductInfo WHERE product2_id = product_id) AS product2_category,
-    customer_count
+    COUNT(*) AS customer_count
 FROM
-    pairs
+    ProductPurchases a
+JOIN
+    ProductPurchases b ON a.user_id = b.user_id
+                        AND a.product_id < b.product_id
+GROUP BY
+    a.product_id,
+    b.product_id
+HAVING
+    COUNT(*) >= 3
 ORDER BY
     customer_count DESC,
     product1_id,
