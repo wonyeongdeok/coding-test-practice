0f3275ba97edaa40f29be8fbbfb0f99e2ba73944
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-07-30 09:20:09 +0900

    Time: 559 ms (85.11%), Space: 0B (100%) - LeetHub

--- LeetCode/3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ LeetCode/3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -5,6 +5,7 @@
 4. order by customer_count desc, product1_id asc, product2_id asc
 */
 
+-- 유저별 상품 쌍 만들기
 WITH pairs AS (
     SELECT
         a.product_id AS product1_id,
@@ -20,41 +21,25 @@ WITH pairs AS (
         b.product_id
     HAVING
         COUNT(*) >= 3
-),
-
-product1 AS (
-    SELECT
-        a.product1_id,
-        a.product2_id,
-        a.customer_count,
-        b.category AS product1_category
-    FROM
-        pairs a
-    LEFT JOIN
-        ProductInfo b ON a.product1_id = b.product_id
-),
-
-product2 AS (
-    SELECT
-        a.product1_id,
-        a.product2_id,
-        a.customer_count,
-        a.product1_category,
-        b.category AS product2_category
-    FROM
-        product1 a
-    LEFT JOIN
-        ProductInfo b On a.product2_id = b.product_id
 )
-
 SELECT
     product1_id,
     product2_id,
-    product1_category,
-    product2_category,
+    (SELECT
+        category
+    FROM
+        ProductInfo
+    WHERE
+        product1_id = product_id) AS product1_category,
+    (SELECT
+        category
+    FROM
+        ProductInfo
+    WHERE
+        product2_id = product_id) AS product2_category,
     customer_count
 FROM
-    product2
+    pairs
 ORDER BY
     customer_count DESC,
     product1_id,
