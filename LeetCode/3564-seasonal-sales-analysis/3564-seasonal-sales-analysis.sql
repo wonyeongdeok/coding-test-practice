d211ed2a8e02a6018b4e9ed8f5d1b6177e428506
Deogi-DA <61719103+wonyeongdeok@users.noreply.github.com>
2025-08-09 15:58:18 +0900

    Time: 525 ms (55.95%), Space: 0B (100%) - LeetHub

--- LeetCode/3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
+++ LeetCode/3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
@@ -1,39 +1,38 @@
+/*
+- find the most popular product category for each season
+- The popularity of a category is determined by the total quantity sold in that season.
+- If there is a tie, select the category with the highest total revenue(quantity * price)
+order by season asc
+*/
+
 WITH seasons AS (
     SELECT
-        CASE WHEN MONTH(a.sale_date) IN (12, 1, 2) THEN 'Winter'
-             WHEN MONTH(a.sale_date) IN (3, 4, 5) THEN 'Spring'
-             WHEN MONTH(a.sale_date) IN (6, 7, 8) THEN 'Summer'
+        product_id,
+        CASE WHEN MONTH(sale_date) IN (12, 1, 2) THEN 'Winter'
+             WHEN MONTH(sale_date) IN (3, 4, 5) THEN 'Spring'
+             WHEN MONTH(sale_date) IN (6, 7, 8) THEN 'Summer'
              ELSE 'Fall'
         END AS season,
-        b.category,
-        a.quantity,
-        a.quantity * a.price AS revenue
+        quantity,
+        quantity * price AS revenue
     FROM
-        sales a
-    JOIN
-        products b ON a.product_id = b.product_id
-),
-quantity_price AS (
-    SELECT
-        season,
-        category,
-        SUM(quantity) AS total_quantity,
-        SUM(revenue) AS total_revenue
-    FROM
-        seasons
-    GROUP BY
-        season,
-        category
+        sales
 ),
 ranking AS (
-SELECT
-    season,
-    category,
-    total_quantity,
-    total_revenue,
-    ROW_NUMBER() OVER(PARTITION BY season ORDER BY total_quantity DESC, total_revenue DESC) AS rn
-FROM
-    quantity_price
+    SELECT
+        a.season,
+        b.category,
+        SUM(a.quantity) AS total_quantity,
+        SUM(a.revenue) AS total_revenue,
+        ROW_NUMBER() OVER(PARTITION BY a.season
+                          ORDER BY SUM(a.quantity) DESC, SUM(a.revenue) DESC) AS RN
+    FROM
+        seasons a
+    JOIN
+        products b ON a.product_id = b.product_id
+    GROUP BY
+        a.season,
+        b.category
 )
 
 SELECT
@@ -44,6 +43,6 @@ SELECT
 FROM
     ranking
 WHERE
-    rn = 1
+    RN = 1
 ORDER BY
     season;
